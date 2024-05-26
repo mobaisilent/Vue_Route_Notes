e5341b571da5a35d580b4ec9875bd64fd59401e5
mobai <mobaisilent@gmail.com>
2024-05-26 23:25:33 +0800

    finished the global front guard

--- dengruicode/project7/src/main.js
+++ dengruicode/project7/src/main.js
@@ -3,5 +3,26 @@ import App from "./App.vue";
 
 import router from "./router";
 // 导入路由器
-createApp(App).use(router).mount("#app");
-// 使用路由器
+// createApp(App).use(router).mount("#app");
+// project7的主要功能就是实现 前置守卫：global front guard
+
+const app = createApp(App); // 这个App 与App.vue之间相互对应：所以大写
+app.use(router);
+
+// 全局前置守卫
+router.beforeEach((to, from, next) => {
+  console.log("to:", to);
+  console.log("form:", from);
+  // 打印查看这两个信息：随便点击几个按钮就知道这是干什么的了
+
+  // next();
+  // 下面设置拦截
+  if (to.name == "member") {
+    next(false);
+  } else {
+    next();
+  }
+  // 编程以及实际演示可见拦截效果
+});
+
+app.mount("#app");
