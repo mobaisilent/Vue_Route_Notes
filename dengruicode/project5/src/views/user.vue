ff65bcfae6ecaa01e5f2a77a24c4b12cf28d008d
mobai <mobaisilent@gmail.com>
2024-05-26 22:26:40 +0800

    finish use router-link, define alias, define route

---
+++ dengruicode/project5/src/views/user.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+</script>
+
+<template>
+  个人主页
+  <br>
+  id:{{ $route.params.id }}
+  <br>
+  name:{{ $route.params.name }}
+  <!-- alt+shift+下实现快速向下复制一行 -->
+</template>
+
+<style scoped></style>
